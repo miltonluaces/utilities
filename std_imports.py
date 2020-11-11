3b44709bef116bed9c69fc952d758cab3c492b2d
miltonluaces <miltonluacs@gmail.com>
2020-11-11 12:32:11 +0000

    updates

---
+++ std_imports.py
@@ -0,0 +1,12 @@
+import os
+os.chdir('D:/source/repos')
+os.getcwd()
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_path = 'D:/data/'
+csv_path = 'D:/data/csv/'
+img_path = 'D:/data/img/'
+text_path = 'D:/data/text/'
