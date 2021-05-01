e9d48ecbaa89065ddee0f7c4467a533dd409a62d
miltonluaces <miltonluacs@gmail.com>
2021-05-01 12:08:20 +0100

    housekeeping

--- tf_imports.py
+++ tf_imports.py
@@ -4,4 +4,5 @@ import tensorflow.keras.utils as tku
 import tensorflow.keras.backend as tkb
 import tensorflow.keras.callbacks as tkc
 import tensorflow.keras.preprocessing as tkp
+import tensorflow.keras.optimizers as tko
 import tensorflow_addons as tfa
